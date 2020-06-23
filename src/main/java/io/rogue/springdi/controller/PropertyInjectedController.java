4277ef51cd38b036237bb7bc0e7eed82fad4bb3c
rogue29 <rohitggulyani@gmail.com>
2020-06-23 12:46:01 +0530

    Use Qualifier and Primary annotations

--- src/main/java/io/rogue/springdi/controller/PropertyInjectedController.java
+++ src/main/java/io/rogue/springdi/controller/PropertyInjectedController.java
@@ -2,10 +2,13 @@ package io.rogue.springdi.controller;
 
 import io.rogue.springdi.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
+
+    @Qualifier("greetingServiceImpl")
     @Autowired
     public GreetingService greetingService;
 
--- src/main/java/io/rogue/springdi/controller/SetterInjectedController.java
+++ src/main/java/io/rogue/springdi/controller/SetterInjectedController.java
@@ -2,13 +2,14 @@ package io.rogue.springdi.controller;
 
 import io.rogue.springdi.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class SetterInjectedController {
     private GreetingService greetingService;
 
-    @Autowired
+    @Autowired @Qualifier("greetingServiceImpl")
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
