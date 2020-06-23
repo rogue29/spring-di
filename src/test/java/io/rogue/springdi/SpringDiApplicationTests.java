37849d197f2862aa923e4f9666fecace309e70c6
rogue29 <rohitggulyani@gmail.com>
2020-06-23 11:07:22 +0530

    Initialized Spring Boot Project

---
+++ src/test/java/io/rogue/springdi/SpringDiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rogue.springdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
